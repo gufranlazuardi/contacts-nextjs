c799f41645cd01ed7833c062db689311d7d03827
gufranlazuardi <gufranlazuardi@gmail.com>
2024-04-12 13:35:23 +0700

    first commit

---
+++ app/components/contact-table.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const ContactTable = () => {
+  return (
+    <table className="w-full text-sm text-left">
+      <thead className="text-sm">
+        <tr>
+          <th>#</th>
+          <th>Name</th>
+          <th>Phone Number</th>
+          <th>Created At</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+export default ContactTable;
---
+++ app/contacts/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className=" max-w-screen-md mx-auto mt-5">
+      <div className="flex items-center justify-between gap-1 mb-5"></div>
+    </div>
+  );
+};
+
+export default Contact;
